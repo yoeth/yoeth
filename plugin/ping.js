29a5982d9736aa789d0c733306d6740afd8a1a3f
idanran <idanran@outlook.com>
2022-02-18 19:02:19 +0800

    pre3

--- plugin/ping.js
+++
@@ -1,7 +0,0 @@
-export default function ping({session}) {
-    console.log(session.alt_message);
-    if (session.alt_message === '天王盖地虎') {
-        console.log("触发")
-        session.send_message('宝塔镇河妖');
-    }
-}
