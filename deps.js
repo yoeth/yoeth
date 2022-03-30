84233ac7e94795ae6ac4123ef33795ca19a6124e
idanran <idanran@outlook.com>
2022-03-30 22:12:54 +0800

    0.0.3

--- deps.js
+++ deps.js
@@ -1,3 +1,4 @@
-export { walk } from "https://deno.land/std@0.126.0/fs/mod.ts";
-export { extname } from "https://deno.land/std@0.126.0/path/mod.ts";
-export { serve } from "https://deno.land/std@0.126.0/http/server.ts";
+export { walk } from "https://deno.land/std@0.132.0/fs/mod.ts";
+export { extname } from "https://deno.land/std@0.132.0/path/mod.ts";
+export { serve } from "https://deno.land/std@0.132.0/http/server.ts";
+export { join } from "https://deno.land/std@0.132.0/path/mod.ts";
